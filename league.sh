2467ff2bddaf06b8eb6dd41194305abbb834749f
haha1082828 <saulovieira1007@gmail.com>
2026-08-29 17:51:27 -0300

    Update league.sh

--- league.sh
+++ league.sh
@@ -144,10 +144,22 @@ league_play() {
                 ;;
             *)
                 if [ "$AVAILABLE_FIGHTS" -eq 0 ]; then
+                    # A recompensa pode ficar indisponivel por algumas horas
+                    # depois da ultima luta. Nunca ficar preso esperando por ela.
                     clickReward=`grep -o -E "/league/takeReward/\?r=[0-9]+" "$TMP/SRC" | sed -n 1p`
+
                     if [ -n "$clickReward" ]; then
-                        fetch_page "$clickReward"
-                        printf "Claimed reward\n"
+                        if fetch_page "$clickReward"; then
+                            printf "Claimed reward\n"
+                        else
+                            printf "League reward request failed; leaving League for now.\n" >> "$TMP/ERROR_DEBUG"
+                            unset click ENEMY_NUMBER PLAYER_STRENGTH E_STRENGTH AVAILABLE_FIGHTS fights_done enemy_index j
+                            return 0
+                        fi
+                    else
+                        printf "League reward not available yet; leaving League for now.\n" >> "$TMP/ERROR_DEBUG"
+                        unset click ENEMY_NUMBER PLAYER_STRENGTH E_STRENGTH AVAILABLE_FIGHTS fights_done enemy_index j
+                        return 0
                     fi
                 fi
                 ;;

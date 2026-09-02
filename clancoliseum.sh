faf70f46793caf071afbbd62e6c15f2dd3718aa0
haha1082828 <saulovieira1007@gmail.com>
2026-09-02 18:17:04 -0300

    Update clancoliseum.sh

--- clancoliseum.sh
+++ clancoliseum.sh
@@ -42,18 +42,7 @@ clancoliseum_fight() {
 
   FIGHT_BREAK=$(($(date +%s) + 600))
   until [ -s "BREAK_LOOP" ] || [ "$(date +%s)" -gt "$FIGHT_BREAK" ]; do
-    if awk -v ush="$(cat USH)" -v hlhp="$(cat HLHP)" 'BEGIN { exit !(ush < hlhp) }' && \
-       [ "$(($(date +%s) - $(cat last_heal)))" -gt 90 ] && \
-       [ "$(($(date +%s) - $(cat last_heal)))" -lt 300 ]; then
-      (
-        run_curl_exec "${URL}$(cat HEAL)" > "$src_ram"
-      ) </dev/null > /dev/null 2>&1 &
-      time_exit 17
-      cf_access
-      cat USH > old_HP
-      date +%s > last_heal
-
-    elif ! grep -q -o 'txt smpl grey' "$src_ram" && \
+    if ! grep -q -o 'txt smpl grey' "$src_ram" && \
          [ "$(($(date +%s) - $(cat last_dodge)))" -gt 20 ] && \
          [ "$(($(date +%s) - $(cat last_dodge)))" -lt 300 ] && \
          awk -v ush="$(cat USH)" -v oldhp="$(cat old_HP)" 'BEGIN { exit !(ush < oldhp) }'; then
@@ -64,6 +53,17 @@ clancoliseum_fight() {
       cf_access
       cat USH > old_HP
       date +%s > last_dodge
+
+    elif awk -v ush="$(cat USH)" -v hlhp="$(cat HLHP)" 'BEGIN { exit !(ush < hlhp) }' && \
+       [ "$(($(date +%s) - $(cat last_heal)))" -gt 90 ] && \
+       [ "$(($(date +%s) - $(cat last_heal)))" -lt 300 ]; then
+      (
+        run_curl_exec "${URL}$(cat HEAL)" > "$src_ram"
+      ) </dev/null > /dev/null 2>&1 &
+      time_exit 17
+      cf_access
+      cat USH > old_HP
+      date +%s > last_heal
 
     elif awk -v latk="$(($(date +%s) - $(cat last_atk)))" -v atktime="$LA" 'BEGIN { exit !(latk != atktime) }' && \
          ! grep -q -o 'txt smpl grey' "$src_ram" && \

fe89b29965e78eba9630e5de316a8b71131b2c06
haha1082828 <saulovieira1007@gmail.com>
2026-09-02 18:18:43 -0300

    Update altars.sh

--- altars.sh
+++ altars.sh
@@ -50,17 +50,7 @@ altars_fight() {
   FIGHT_BREAK=$(($(date +%s) + 600))
   until [ -s "BREAK_LOOP" ] || [ "$(date +%s)" -gt "$FIGHT_BREAK" ]; do
     cf_access
-    if awk -v ush="$(cat HP)" -v hlhp="$(cat HLHP)" 'BEGIN { exit !(ush < hlhp) }' && \
-         [ "$(($(date +%s) - $(cat last_heal)))" -gt 90 ] && \
-         [ "$(($(date +%s) - $(cat last_heal)))" -lt 300 ]; then
-      (
-        run_curl_exec "${URL}$(cat HEAL)" > "$TMP/src.html"
-      ) </dev/null > /dev/null 2>&1 &
-      time_exit 17
-      cf_access
-      cat HP > FULL; cat HP > old_HP
-      date +%s > last_heal
-    elif ! grep -q -o 'txt smpl grey' "$TMP/src.html" && \
+    if ! grep -q -o 'txt smpl grey' "$TMP/src.html" && \
        [ "$(($(date +%s) - $(cat last_dodge)))" -gt 20 ] && \
        [ "$(($(date +%s) - $(cat last_dodge)))" -lt 300 ] && \
        awk -v ush="$(cat HP)" -v oldhp="$(cat old_HP)" 'BEGIN { exit !(ush < oldhp) }'; then
@@ -70,6 +60,17 @@ altars_fight() {
       time_exit 17
       cf_access
       cat HP > old_HP; date +%s > last_dodge
+
+    elif awk -v ush="$(cat HP)" -v hlhp="$(cat HLHP)" 'BEGIN { exit !(ush < hlhp) }' && \
+         [ "$(($(date +%s) - $(cat last_heal)))" -gt 90 ] && \
+         [ "$(($(date +%s) - $(cat last_heal)))" -lt 300 ]; then
+      (
+        run_curl_exec "${URL}$(cat HEAL)" > "$TMP/src.html"
+      ) </dev/null > /dev/null 2>&1 &
+      time_exit 17
+      cf_access
+      cat HP > FULL; cat HP > old_HP
+      date +%s > last_heal
 
     elif awk -v latk="$(($(date +%s) - $(cat last_atk)))" -v atktime="$LA" 'BEGIN { exit !(latk != atktime) }' && \
          ! grep -q -o 'txt smpl grey' "$TMP/src.html" && \

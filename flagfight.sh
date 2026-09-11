2c84c30c13d62d5ef031e4311621a9115e6938ac
haha1082828 <saulovieira1007@gmail.com>
2026-09-11 18:22:35 -0300

    Update flagfight.sh

--- flagfight.sh
+++ flagfight.sh
@@ -7,18 +7,24 @@ flagfight_fight() {
   LA=4
   HPER=40
   RPER=15
+  
+  rm -f "$full_ram"
 
   cf_access() {
     [ -f "$src_ram" ] || return 1
-    grep -o -E '(/[a-z]+/[a-z]{0,4}at[a-z]{0,3}k/[?]r[=][0-9]+)' "$src_ram" | sed -n '1p' > ATK 2>/dev/null
-    grep -o -E '(/[a-z]+/at[a-z]{0,3}k[a-z]{3,6}/[?]r[=][0-9]+)' "$src_ram" | sed -n 1p > ATKRND 2>/dev/null
-    grep -o -E '(/flagfight/dodge/[?]r[=][0-9]+)' "$src_ram" | sed -n 1p > DODGE 2>/dev/null
-    grep -o -E '(/flagfight/heal/[?]r[=][0-9]+)' "$src_ram" | sed -n 1p > HEAL 2>/dev/null
-    grep -o -E '(/[a-z]+/shield/[?]r[=][0-9]+)' "$src_ram" | sed -n 1p > SHIELD 2>/dev/null
+    grep -o -E '(/[a-z]+/[a-z]{0,4}at[a-z]{0,3}k/?[?]r[=][0-9]+)' "$src_ram" | sed -n '1p' > ATK 2>/dev/null
+    grep -o -E '(/[a-z]+/at[a-z]{0,3}k[a-z]{3,6}/?[?]r[=][0-9]+)' "$src_ram" | sed -n 1p > ATKRND 2>/dev/null
+    grep -o -E '(/flagfight/dodge/?[?]r[=][0-9]+)' "$src_ram" | sed -n 1p > DODGE 2>/dev/null
+    grep -o -E '(/flagfight/heal/?[?]r[=][0-9]+)' "$src_ram" | sed -n 1p > HEAL 2>/dev/null
+    grep -o -E '(/[a-z]+/shield/?[?]r[=][0-9]+)' "$src_ram" | sed -n 1p > SHIELD 2>/dev/null
     grep -o -E '([[:upper:]][[:lower:]]{0,20}( [[:upper:]][[:lower:]]{0,17})?)[[:space:]]\(' "$src_ram" | sed -n 's,\ [(],,;s,\ ,_,;2p' > CLAN 2>/dev/null
     grep -o -E '([[:upper:]][[:lower:]]{0,15}( [[:upper:]][[:lower:]]{0,13})?)[[:space:]][^[:alnum:]]s' "$src_ram" | sed -n 's,\ [<]s,,;s,\ ,_,;2p' > USER 2>/dev/null
     grep -o -E "(hp)[^A-Za-z0-9]{1,4}[0-9]{1,6}" "$src_ram" | grep -o -E '[0-9]+' | head -n 1 > USH 2>/dev/null
     grep -o -E "(nbsp)[^A-Za-z0-9]{1,2}[0-9]{1,6}" "$src_ram" | grep -o -E '[0-9]+' | head -n 1 > ENH 2>/dev/null
+
+    if [ ! -s "$full_ram" ]; then
+      cat USH > "$full_ram" 2>/dev/null
+    fi
 
     read -r ush < USH 2>/dev/null
     read -r full < "$full_ram" 2>/dev/null
@@ -54,7 +60,6 @@ flagfight_fight() {
       ) </dev/null > /dev/null 2>&1 &
       time_exit 17
       cf_access
-      cat USH > "$full_ram"
       cat USH > old_HP
       date +%s > last_heal
       FIRST_HEAL=0
@@ -112,10 +117,6 @@ flagfight_start() {
 
   case `date +%H:%M` in
   (10:1[0-4]|16:1[0-4])
-    (
-      run_curl_exec "$URL/train" | grep -o -E '\(([0-9]+)\)' | sed 's/[()]//g' > "$full_ram"
-    ) </dev/null > /dev/null 2>&1 &
-    time_exit 17
     fetch_page "/flagfight/?close=reward" "$src_ram"
     fetch_page "/flagfight/enterFight" "$src_ram"
     printf "Flagfight will be started...\n"
@@ -128,7 +129,7 @@ flagfight_start() {
       run_curl_exec "$URL/flagfight/enterFight" > "$src_ram"
     ) </dev/null > /dev/null 2>&1 &
     time_exit 17
-    grep -o -E '(/[a-z]+/[a-z]+/[^A-Za-z0-9]r[^A-Za-z0-9][0-9]+)' "$src_ram" | sed -n '1p' > "$TMP/ACCESS" 2>/dev/null
+    grep -o -E '(/[a-z]+/[a-z]+/?[?]r[=][0-9]+)' "$src_ram" | sed -n '1p' > "$TMP/ACCESS" 2>/dev/null
     printf " Entering...\n"
     printf " Waiting...\n"
 
@@ -137,7 +138,7 @@ flagfight_start() {
     until grep -q -o 'flagfight/dodge/' "$TMP/ACCESS" || [ "$(date +%s)" -gt "$BREAK" ]; do
       printf " ...\n%s\n" "`cat "$TMP/ACCESS"`"
       fetch_page "/flagfight/" "$src_ram"
-      grep -o -E '(/flagfight/[a-z]+/[^A-Za-z0-9]r[^A-Za-z0-9][0-9]+)' "$src_ram" | sed -n '1p' > "$TMP/ACCESS" 2>/dev/null
+      grep -o -E '(/flagfight/[a-z]+/?[?]r[=][0-9]+)' "$src_ram" | sed -n '1p' > "$TMP/ACCESS" 2>/dev/null
       sleep 3
     done
 
